545e6e0652f0a8a96b33b30561364139ed7210e0
DeyanSpasovBerlin <pislpe@gmail.com>
2024-10-15 22:06:22 +0200

    Add files via upload

--- example/Main.java
+++ example/Main.java
@@ -1,10 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
         ArraySort arrSort = new ArraySort();
-        Integer[] array1 = {8,15,6,28,14,3,7};
-        Integer[] array2 = {111,100,47,99,5,-55,7,8,8,8,3};
+        int[] array1 = {8,15,6,28,14,3,7};
+        int[] array2 = {111,100,47,99,5,-55,7,8,8,8,3};
         arrSort.sortArray(array1);
         arrSort.sortArray(array2);
 
@@ -13,5 +15,22 @@ public class Main {
         matrix.print2DArray(matrix.generateMatrix(4,3));
         matrix.print2DArray(matrix.generateMatrix(3,4));
 
+        MaxColumn mxcl = new MaxColumn();
+
+        int[][] randMatr = mxcl.generateMatrix();// generate random matrix
+        matrix.print2DArray(randMatr);
+
+        int[][] randMatrSortRows =  mxcl.sortRows(randMatr);//sort matrix by rows
+        matrix.print2DArray(randMatrSortRows);
+
+        int[] arrMaxRows = mxcl.maxElementRowsArr(randMatrSortRows);//make array from max element in each rows
+        System.out.println("arrMaxRows =" + Arrays.toString(arrMaxRows));
+
+        int[][] randMatrSortColumns =  mxcl.sortColumn(randMatr);//sort by coluns
+        matrix.print2DArray(randMatrSortColumns);
+
+
+        int[] arrMaxColumns = mxcl.maxElementColumnsArr(randMatrSortColumns);//make array from max element in each colunss
+        System.out.println("arrMaxColumns =" + Arrays.toString(arrMaxColumns));
     }
 }
